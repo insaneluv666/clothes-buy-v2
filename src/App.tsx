82197a26d30c64dabe3757b1695df96966a39d98
tyomzya <tyomzya@mail.ru>
2024-08-20 14:39:08 +0300

    Added Router + Empty Pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,35 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import styled from "styled-components"
+
+import Header from "./components/Header"
+import Home from "./pages/Home"
+import Products from "./pages/Products"
+import Product from "./pages/Product"
+import Cart from "./pages/Cart"
+import Profile from "./pages/Profile"
+import Footer from "./components/Footer"
+
+
 
 function App() {
   return (
-    <>
-     
-    </>
+      <Router>
+        <Header />
+          <Routes>
+
+            <Route path="/" element={<Home />} />
+            <Route path="/products" element={<Products />} />
+            <Route path="/products/product" element={<Product />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/profile" element={<Profile />} />
+
+          </Routes>
+        <Footer />
+      </Router>
   )
 }
+
+
+
 
 export default App
