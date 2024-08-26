3633dd9d52371b8cd5f5abdd183ba78adfd51661
tyomzya <tyomzya@mail.ru>
2024-08-26 18:03:58 +0300

    Footer update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,22 @@ const Footer = () => {
   return ( 
     <FooterWrapper>
       <FooterContent>
-        Все права защищены &copy;
+        <Address>
+          <h4>Адрес магазина</h4>
+          <span>Цветной б-р, 15 стр. 1, Москва, 127051</span>
+          <span>Универмаг "Цветной", 7 этаж</span>
+        </Address>
+        <Socials>
+          <h4>Социальные сети</h4>
+          <a href="#">Telegram</a>
+          <a href="#">ВКонтакте</a>
+        </Socials>
+        <Contacts>
+          <h4>Контакты</h4>
+          <a href="mailto:support@clothesbuy.ru">support@clothesbuy.ru</a>
+          <a href="tel:+79779779797">+7 (977) 977-97-97</a>
+          <span>Ежедневно с 9:00 до 22:00</span>
+        </Contacts>
       </FooterContent>
     </FooterWrapper>
    );
@@ -12,13 +27,37 @@ const Footer = () => {
  
 const FooterWrapper = styled.div`
   background-color: #303030;
-  height: 80px;
   display: flex;
   justify-content: center;
   align-items: center;
 `
 const FooterContent = styled.div`
-  margin: 10px 50px;
+  display: flex;
+  margin: 30px 0;
+  width: 90%;
+  flex-direction: row;
+  justify-content: space-between;
+  h4{
+    margin-bottom: 12px;
+  }
+  span{
+    margin-bottom: 8px;
+  }
+  a{
+    margin-bottom: 8px;
+  }
+`
+const Address = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+const Socials = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+const Contacts = styled.div`
+  display: flex;
+  flex-direction: column;
 `
 
 
